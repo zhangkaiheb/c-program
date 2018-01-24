351e1c1cdf6c590c7f6cac79464a76e6c83909f5
zhangkaiheb <zhangkaiheb@126.com>
2018-01-24 12:19:46 +0800

    update

--- udpsock/udpsock_c.c
+++ udpsock/udpsock_c.c
@@ -12,51 +12,53 @@
 int main(int arg, char * args[])
 {
 	socklen_t addrlen;
-    char buf[2048] = {0};
-    struct sockaddr_in addr;
+	char buf[2048] = {0};
+	struct sockaddr_in addr;
 
-    if (arg < 3) {
-        printf("please input: ip port!\n");
-        return -1;
-    }
-    int port = atoi(args[2]);
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
-        printf("create socket failed ! error message :%s\n", strerror(errno));
-        return -1;
-    }
-    /*
-    int on = 1;
-    if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on)) == -1) {
-        printf("setsockopt failed ! error message :%s\n", strerror(errno));
-        return -1;
-    }
-    */
-    bzero(&addr, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = inet_addr(args[1]);
+	if (arg < 3) {
+		printf("please input: ip port!\n");
+		return -1;
+	}
+	int port = atoi(args[2]);
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		printf("create socket failed ! error message :%s\n", strerror(errno));
+		return -1;
+	}
+	/*
+	   int on = 1;
+	   if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on)) == -1) {
+	   printf("setsockopt failed ! error message :%s\n", strerror(errno));
+	   return -1;
+	   }
+	 */
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = inet_addr(args[1]);
 
-    while (1) {
-        //read
-        read(STDIN_FILENO, buf, sizeof(buf));
+	while (1) {
+		bzero(buf, sizeof(buf));
+		//read
+		if (read(STDIN_FILENO, buf, sizeof(buf)) == -1)
+			continue;
 
-        if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *) &addr,
-                sizeof(addr)) == -1) {
-            printf("sendto failed! error:%s\n", strerror(errno));
-            break;
-        }
-        memset(buf,0,sizeof(buf));
+		if (sendto(fd, buf, strlen(buf), 0, (struct sockaddr *) &addr,
+					sizeof(addr)) == -1) {
+			printf("sendto failed! %s\n", strerror(errno));
+			break;
+		}
 
-		if (recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &addrlen) == -1) {
-            printf("recvfrom failed! error message :%s\n", strerror(errno));
+		if (recvfrom(fd, buf, sizeof(buf), 0,
+					(struct sockaddr *)&addr, &addrlen) == -1) {
+			printf("recvfrom failed! :%s\n", strerror(errno));
 			break;
 		} else {
 			printf("received (%s)\n", buf);
 		}
-    }
-    close(fd);
+	}
+	close(fd);
 
-    return 0;
+	return 0;
 }
 
--- udpsock/udpsock_s.c
+++ udpsock/udpsock_s.c
@@ -11,52 +11,56 @@
 
 int main(int arg, char * args[])
 {
-    char buf[2048]={0};
-	char result[] = "success";
-    struct sockaddr_in addr;
-    struct sockaddr_in client_addr;
-    socklen_t addrlen = sizeof(client_addr);
+	int ret;
+	char buf[2048]={0};
+	struct sockaddr_in addr;
+	struct sockaddr_in from;
+	socklen_t fromlen = sizeof(from);
+	const char result[] = "success";
 
-    if (arg < 2) {
-        printf("please input: port!\n");
-        return -1;
-    }
+	if (arg < 2) {
+		printf("please input: port!\n");
+		return -1;
+	}
 
-    int port = atoi(args[1]);
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
-        printf("open socket failed ! error: %s\n", strerror(errno));
-        return -1;
-    }
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(port);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    //bind port
-    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-        printf("bind IP failed ! error: %s\n", strerror(errno));
-        goto out;
-    }
+	int port = atoi(args[1]);
+	int fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd == -1) {
+		printf("open socket failed ! %s\n", strerror(errno));
+		return -1;
+	}
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	//bind port
+	if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		printf("bind IP failed ! %s\n", strerror(errno));
+		goto out;
+	}
 
 	while(1) {
-		memset(&client_addr, 0, sizeof(client_addr));
-		if (recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&client_addr, &addrlen)==-1) {
-			printf("recvfrom failed ! error: %s\n",strerror(errno));
+		bzero(buf, sizeof(buf));		
+		bzero(&from, sizeof(from));
+
+		if ((ret = recvfrom(fd, buf, sizeof(buf), 0,
+					(struct sockaddr *)&from, &fromlen)) == -1) {
+			printf("recvfrom failed ! %s\n", strerror(errno));
 			goto out;
 		} else {
-			printf("from %s:%s\n",inet_ntoa(client_addr.sin_addr), buf);
+			printf("Recv %d from %s:%s\n", ret, inet_ntoa(from.sin_addr), buf);
 
-			if (sendto(fd, result, sizeof(result), 0, (struct sockaddr *)&client_addr, addrlen) == -1) {
-				printf("sendto failed ! error: %s\n", strerror(errno));
+			if (sendto(fd, result, sizeof(result), 0,
+					(struct sockaddr *)&from, fromlen) == -1) {
+				printf("sendto failed ! %s\n", strerror(errno));
 				goto out;
 			}
 		}
-		bzero(buf, sizeof(buf));		
 	}
 
 out:
 	close(fd);
 
-    return 0;
+	return -1;
 }
 
 
